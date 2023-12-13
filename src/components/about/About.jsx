61c9842d3ff5dfd489316a06b7ae348ecd3285e9
Blvk Holmes <78100467+Bernard-Baah@users.noreply.github.com>
2023-12-13 15:56:05 -0500

    images fixes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
             <h2>About Me</h2>
 
             <div className="container about__container">
-                <div className="about me">
+                <div className="about__me">
                     <div className="about__me-image">
                         <img src={ME} alt="About me" />
                     </div>
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
 return (
 <div className='cta'>
-<a href="">Download CV</a>
+<a href="">Download Resume</a>
 <a href="#contact">Let's Talk</a>
 </div>
 )
